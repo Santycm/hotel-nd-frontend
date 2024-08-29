c23fc46408e8f73dcddc2d5bf5535bb36dbb879a
Santycm <santicmoreno@gmail.com>
2024-08-28 21:57:50 -0500

    initial files

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import {API_URL} from "./config.js";
+
+export const App = () => {
+  const [dbTest, setDBTest] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch(
+        `${API_URL}/testdb`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await response.json();
+      setDBTest([...data]);
+    };
+    getData();
+  }, []);
+
+  return (
+    <>
+      <h1 className="text-[20px] text-black">HORA DESDE LA DB</h1>
+      {dbTest.map((item) => {
+        return (
+          <div key={item.now}>
+            <p>{item.now}</p>
+          </div>
+        );
+      })}
+    </>
+  );
+};
