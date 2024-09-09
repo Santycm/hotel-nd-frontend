436a82f0a7367af5f654824fa505acd46d31dcda
Santycm <santicmoreno@gmail.com>
2024-09-09 18:33:21 -0500

    fix routes

--- src/routes/Rutas.jsx
+++ src/routes/Rutas.jsx
@@ -1,24 +1,20 @@
 /* Aca van todas las rutas de la web donde desde el Login y Register y Home y toda la navegacion
 donde despues lo importamos al App.js para mejorar la modularidad
-*/  
+*/
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Login } from "../screens/Login/Login";
-import { Register } from "../screens/Register/Register";
-
-
-
+import { Login } from "../screens/login/Login.jsx";
+import { Register } from "../screens/register/Register.jsx";
 
 function Rutas() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Login/>}/>
-                <Route path="/register" element={<Register/>}/>
-            
-            </Routes>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default Rutas;
