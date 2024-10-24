e516343956380f1036ec9b68fbe504b780eeb37c
yeyeGL <gyeisson38@gmail.com>
2024-10-24 14:34:44 -0500

    verificando el token de reservas

--- src/screens/reservation/Reservation.jsx
+++ src/screens/reservation/Reservation.jsx
@@ -4,8 +4,6 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 
-//Falta algunas cosas para que pueda mandar los datos al backend
-
 const Reservation = () => {
   const navigate = useNavigate();
   const [isSubmitting, setIsSubmitting] = useState(false);
@@ -19,7 +17,12 @@ const Reservation = () => {
   const onSubmit = async (data) => {
     setIsSubmitting(true);
     try {
-      const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/createReservation`, data);
+      const token = localStorage.getItem("token");
+      const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/create-reservation`, data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
       console.log(response.data);
       reset();
       navigate("/verreservas");
@@ -29,11 +32,10 @@ const Reservation = () => {
       setIsSubmitting(false);
     }
   };
-
+  
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-contentColor to-gray-800 p-5">
       <div className="bg-white p-10 rounded-lg shadow-lg w-full max-w-md">
-        {/*Titulo y descripcion de la pagina*/}
         <h1 className="text-3xl font-extrabold mb-4 text-center text-contentColor">
           Reserva tu habitacion
         </h1>
@@ -47,28 +49,24 @@ const Reservation = () => {
 
           {/*Input de email de reserva*/}
           <div className="mb-4">
-            <label className="block text-sm font-medium mb-1 text-gray-800" htmlFor="date">
+            <label className="block text-sm font-medium mb-1 text-gray-800" htmlFor="user_email">
               <MailIcon className="h-5 w-5 inline-block mr-1 text-contentColor" />Email para la reserva
             </label>
             <input
               type="email"
               id="user_email"
               placeholder="Email"
-              {...register("user_email", { required: "El emial es requerido" })}
+              {...register("user_email", { required: "El email es requerido" })}
               className="block w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-contentColor placeholder:text-black"
             />
-
             {errors.user_email && (
-              <span className="text-red-500 text-sm">
-                {errors.user_email.message}
-              </span>
+              <span className="text-red-500 text-sm">{errors.user_email.message}</span>
             )}
           </div>
 
           {/*Input de fecha de reserva*/}
           <div className="mb-4">
-            <label
-              className="block text-sm font-medium mb-1 text-gray-800" htmlFor="date">
+            <label className="block text-sm font-medium mb-1 text-gray-800" htmlFor="date">
               <CalendarIcon className="h-5 w-5 inline-block mr-1 text-contentColor" />Fecha de Reserva
             </label>
             <input
@@ -78,33 +76,27 @@ const Reservation = () => {
               className="block w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-contentColor"
             />
             {errors.date && (
-              <span className="text-red-500 text-sm">
-                {errors.date.message}
-              </span>
+              <span className="text-red-500 text-sm">{errors.date.message}</span>
             )}
           </div>
+          
           {/*Input de tipo de habitacion de reserva*/}
           <div className="mb-4">
-            <label className="block text-sm font-medium mb-1 text-gray-800" htmlFor="roomType" >
+            <label className="block text-sm font-medium mb-1 text-gray-800" htmlFor="id_suite">
               <UsersIcon className="h-5 w-5 inline-block mr-1 text-contentColor" />Tipo de Habitacion
             </label>
             <select
-              id="suite_name"
-              {...register("suite_name", {
-                required: "El tipo de habitacion es requerido",
-              })}
+              id="id_suite"
+              {...register("id_suite", { required: "El tipo de habitacion es requerido" })}
               className="block w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-contentColor"
             >
               <option value="">Seleccione un tipo</option>
-              <option value="individual">Individual</option>
-              <option value="doble">Doble</option>
-              <option value="suite">Suite</option>
+              <option value="1">Individual</option>
+              <option value="2">Doble</option>
+              <option value="3">Suite</option>
             </select>
-
-            {errors.suite_name && (
-              <span className="text-red-500 text-sm">
-                {errors.suite_name.message}
-              </span>
+            {errors.id_suite && (
+              <span className="text-red-500 text-sm">{errors.id_suite.message}</span>
             )}
           </div>
 
